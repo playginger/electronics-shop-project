ee20485f94a13670dfdd654bc516134cd75ab57d
Серж Рыж <126515806+playginger@users.noreply.github.com>
2023-04-23 02:59:10 +0300

    commit

--- tests/test_item.py
+++ tests/test_item.py
@@ -2,39 +2,52 @@
 
 from src.item import Item
 
-item1 = Item("Смартфон", 10000, 20)
-item2 = Item("Ноутбук", 20000, 5)
-assert item1.total_price() == 200000
-assert item2.total_price() == 100000
 
-Item.discount = 0.9  # установка скидки на все товары
-item1 = Item("Смартфон", 10000, 20)
-item2 = Item("Ноутбук", 20000, 5)
+def test_total_price():
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
+    assert item1.total_price() == 200000
+    assert item2.total_price() == 100000
 
-assert item1.price == 10000  # ожидаемая цена со ставкой оплаты
-assert item2.price == 20000  # ожидаемая цена со ставкой оплаты
 
-Item.discount = -0.1  # отрицательная скидка
-item1 = Item("Смартфон", 10000, 20)
-item2 = Item("Ноутбук", 20000, 5)
-item1.apply_pay_rate(0.1)
-assert item1.price == 9000
-assert item2.price == 20000
+def test_apply_pay_rate():
+    Item.discount = 0.9  # установка скидки на все товары
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
+    assert item1.price == 10000  # ожидаемая цена со ставкой оплаты
+    assert item2.price == 20000  # ожидаемая цена со ставкой оплаты
 
-Item.pay_rate = 0.5  # положительный коэффициент оплаты
-item1 = Item("Смартфон", 10000, 20)
-item2 = Item("Ноутбук", 20000, 5)
-item1.apply_pay_rate(Item.pay_rate)  # передать pay_rate как аргумент
-assert item1.price == 5000 and item2.price == 20000
 
-item = Item('Смартфон', 10000, 1)
-item.name = 'Ноутбук'
-assert item.name == 'Ноутбук'
+def test_apply_pay_rate_negative_pay_rate():
+    Item.discount = -0.1  # отрицательная скидка
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
+    item1.apply_pay_rate(0.1)
+    assert item1.price == 9000
+    assert item2.price == 20000
 
-item1 = Item('Смартфон', 10000, 1)
-item1.instantiate_from_csv()
-assert len(Item.all) == 20
-assert Item.all[0].name == 'Смартфон'
 
-item = Item('Смартфон', 10000, 1)
-assert isinstance(item.string_to_number(item.quantity), int)
+def test_apply_pay_rate_positive_pay_rate():
+    Item.pay_rate = 0.5  # положительный коэффициент оплаты
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
+    item1.apply_pay_rate(Item.pay_rate)  # передать pay_rate как аргумент
+    assert item1.price == 5000 and item2.price == 20000
+
+
+def test_name_setter():
+    item = Item('Смартфон', 10000, 1)
+    item.name = 'Ноутбук'
+    assert item.name == 'Ноутбук'
+
+
+def test_instantiate_from_csv():
+    item1 = Item('Смартфон', 10000, 1)
+    item1.instantiate_from_csv()
+    assert len(Item.all) == 20
+    assert Item.all[0].name == 'Смартфон'
+
+
+def test_string_to_number():
+    item = Item('Смартфон', 10000, 1)
+    assert isinstance(item.string_to_number(item.quantity), int)
