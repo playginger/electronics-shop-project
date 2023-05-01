282d44fd042428389644e7d44eb6a99681d678da
Серж Рыж <126515806+playginger@users.noreply.github.com>
2023-05-01 18:21:04 +0300

    commit

--- tests/test_phone.py
+++ tests/test_phone.py
@@ -16,12 +16,13 @@ def test_methods():
 
 def test_add():
     item1 = Item("Смартфон", 10000, 20)
-    phone = Phone("iPhone 14", 120_000, 5, 2)
-    assert item1 + phone == 25
-    assert phone + phone == 10
+    phone1 = Phone("iPhone 14", 120_000, 5, 2)
+    assert item1 + phone1 == 25
+    assert phone1 + phone1 == 10
 
 
-def test_setter(phone):
-    assert phone.number_of_sim == 2
+def test_setter():
+    phone1 = Phone("iPhone 14", 120_000, 5, 2)
+    assert phone1.number_of_sim == 2
     with pytest.raises(ValueError):
-        phone.number_of_sim = 0
+        phone1.number_of_sim = 0
